a29bf6edb08deabd629bb8c4bdb71b3285260a46
Xinyu <xinyuya2@login-gpu.cm.cluster>
2025-01-30 14:57:47 -0500

    [Major] Release Code for APE

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+__version__ = '0.0.0'
+
+setup(
+    name="ape",
+    version=__version__,
+    author="Xinyu Yang",
+    author_email='xinyuya2@andrew.cmu.edu',
+    url='',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
