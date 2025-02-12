a5bd1073dfe1abb499ea91348218da0f9a1489a4
Xinyu <xinyuya2@login-gpu.cm.cluster>
2025-02-12 00:02:10 -0500

    update

--- demo_ape.py
+++ demo_ape.py
@@ -58,24 +58,24 @@ def enable_attention_prefill_prefix(model_name, model):
 
 def enable_attention_prefill_context(model_name, model):
     if "llama" in args.model:
-        from src.ape_llama import enable_llama_attention_prefill_context
+        from ape.ape_llama import enable_llama_attention_prefill_context
         enable_llama_attention_prefill_context(model)
     elif "mistral" in model_name:
-        from src.ape_mistral import enable_mistral_attention_prefill_context
+        from ape.ape_mistral import enable_mistral_attention_prefill_context
         enable_mistral_attention_prefill_context(model)
     elif "gemma" in model_name:
-        from src.ape_gemma import enable_gemma_attention_prefill_context
+        from ape.ape_gemma import enable_gemma_attention_prefill_context
         enable_gemma_attention_prefill_context(model)
 
 def enable_attention_prefill_query(model_name, model, temperature, scale):
     if "llama" in args.model:
-        from src.ape_llama import enable_llama_attention_prefill_query
+        from ape.ape_llama import enable_llama_attention_prefill_query
         enable_llama_attention_prefill_query(model, temperature, scale)
     elif "mistral" in model_name:
-        from src.ape_mistral import enable_mistral_attention_prefill_query
+        from ape.ape_mistral import enable_mistral_attention_prefill_query
         enable_mistral_attention_prefill_query(model, temperature, scale)
     elif "gemma" in model_name:
-        from src.ape_gemma import enable_gemma_attention_prefill_query
+        from ape.ape_gemma import enable_gemma_attention_prefill_query
         enable_gemma_attention_prefill_query(model, temperature, scale)
 
 def seed_everything(seed):
